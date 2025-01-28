0bbb89edc76937041eeeb52397ede5ff799af2e7
ajkoi <felix.yiou.alias.yeou@gmail.com>
2025-01-28 09:53:31 +0100

    updates sur le main

--- main.py
+++ main.py
@@ -10,13 +10,15 @@ grille = [[0 for i in range(10)] for j in range(20)]
 def afficher_grille(grille):
     """pour afficher la grille dans l'invite de commande"""
     for ligne in grille:
-        print(ligne)
-
+        print("| ", end="")
+        for char in ligne:
+            print(char, end=" ")
+        print("|")
 afficher_grille(grille)
 forme_possibles = [[[1, 1, 1, 1]],
                     [[2, 2], [2, 2]],
                     [[0, 3], [3, 3], [3, 0]],
-                    [[0, 4], [4, 4], [0, 4]],
+                    [[4, 0], [4, 4], [0, 4]],
                     [[5, 0], [5, 5], [0, 5]]
                    ]
 class Piece:
@@ -25,9 +27,12 @@ class Piece:
         self.forme = forme_possibles[forme]
         self.x = x
         self.y = y
-        def rotation(self):
-        """Faire une rotation à la pièce"""
+    def rotation(self):
+    """Faire une rotation à la pièce"""
 def main():
       """La boucle principale du jeu"""
-      afficher_grille(grille):
-
+      afficher_grille(grille)
+# pour inserer une pièce en haut
+for lignes in range(len(piece)):
+    for char in range(len(piece[lignes])):
+        grille[lignes][char+5] = piece[lignes][char]
